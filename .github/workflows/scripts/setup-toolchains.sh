8fec8903464c699f12b5630c00832fd6c127b942
ThatMG393 <thatmg393@gmail.com>
2024-01-30 07:06:25 +0800

    Hope it builds 💀🙏

--- .github/workflows/scripts/setup-toolchains.sh
+++ .github/workflows/scripts/setup-toolchains.sh
@@ -7,7 +7,14 @@ sudo apt install bison flex libssl-dev bc build-essential libncurses5-dev g++-mu
 
 if [ "$1" -e "armeabi-v7a" ]; then
 	echo "Installing 'armeabi-v7a' toolchain.."
-	sudo apt install libc6-armhf-cross libc6-dev-armhf-cross binutils-arm-linux-gnueabi -y
+	sudo apt install \
+		gcc-4.7-arm-linux-gnueabihf \
+		gcc-4.7-multilib-arm-linux-gnueabihf \
+		binutils-arm-linux-gnueabihf \
+		libgcc1-armhf-cross \
+		libsfgcc1-armhf-cross \
+		libstdc++6-armhf-cross \
+		libstdc++6-4.7-dev-armhf-cross -y
 else
 	echo "Installing 'arm64-v8a' toolchain..."
 	sudo apt install libc6-arm64-cross libc6-dev-arm64-cross binutils-arm64-linux-gnueabi -y
